815c7bcaad3bd9697d90d79d9c8740c0748dcd72
RyanRanjith <kingranjith4@gmail.com>
2024-08-20 08:58:11 -0700

    a

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sideMenu = document.querySelector('#sideMenu');
-const navBar = document.querySelector('nav');
-const navBar = document.querySelector('nav');
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+
 
 
 function openMenu() {
@@ -10,8 +11,14 @@ function closeMenu() {
     sideMenu.style.transform = 'translateX(16rem)'
 }
 
-window.addEventListener('scroll', ()=>{
+window.addEventListener('scroll',()=>{
     if(scrollY > 50){
-
+      navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg',
+     'shadow-sm')
+     navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    }else{
+        navBar.classList.remove ('bg-white', 'bg-opacity-50', 'backdrop-blur-lg',
+            'shadow-sm')
+            navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
     }
 })
